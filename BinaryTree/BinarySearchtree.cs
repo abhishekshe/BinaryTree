4a589939d08896e81648fd5e436f78746eb8014c
Abhishek Narwariya <abhisheknarwariya813@gmail.com>
2022-05-05 13:23:29 +0530

    Add UC2

--- BinaryTree/BinarySearchtree.cs
+++ BinaryTree/BinarySearchtree.cs
@@ -4,35 +4,39 @@ using System.Text;
 
 namespace BinarySearchTree
 {
+   
     class BinarySearchTree<T> where T : IComparable
     {
-        //creating a object of Node class, called root which will save the first data coming from the user
+        
         public Node<T> root;
+        int leftNodeCount = 0;
+        int rightNodeCount = 0;
 
-       
+  
         public void InsertDataInBST(T data)
         {
             
             Node<T> node = new Node<T>(data);
 
-            //when there is no initial data and root is null, first value is added in root.
+            
             if (root == null)
             {
                 root = node;
             }
             else
             {
-                //current position node is defined which have the value of root element.
+                
                 Node<T> currentPosition = root;
-                //loop is instatiated to add value in binary search tree
+            
                 while (true)
                 {
+                    
                     Node<T> parent = currentPosition;
                     
                     if (currentPosition.data.CompareTo(data) > 0)
                     {
                         currentPosition = currentPosition.left;
-                        
+                       
                         if (currentPosition == null)
                         {
                             parent.left = node;
@@ -52,14 +56,14 @@ namespace BinarySearchTree
                 }
             }
         }
-      
+        
         public void DisplayDatainBST()
         {
-           
+          
             Node<T> parent = root;
-           
             if (root.left != null)
             {
+                leftNodeCount++;
                 parent = root;
                 root = root.left;
                 DisplayDatainBST();
@@ -68,9 +72,17 @@ namespace BinarySearchTree
            
             if (root.right != null)
             {
+                rightNodeCount++;
                 root = root.right;
                 DisplayDatainBST();
             }
+        }
+     
+        public void CountOfNodes()
+        {
+            
+            Console.WriteLine("Count for Left nodes are\t" + leftNodeCount);
+            Console.WriteLine("Count for Right nodes are\t" + rightNodeCount);
         }
 
     }
