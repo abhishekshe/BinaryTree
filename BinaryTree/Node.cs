e539364c8c1577a23cc62fe2e4a52d4b75ea7732
Abhishek Narwariya <abhisheknarwariya813@gmail.com>
2022-05-05 13:10:47 +0530

    Add UC1

---
+++ BinaryTree/Node.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinarySearchTree
+{
+  
+    class Node<T>
+    {
+        public T data;
+        public Node<T> left;
+        public Node<T> right;
+
+     
+        public Node(T data)
+        {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+
+    }
+}
